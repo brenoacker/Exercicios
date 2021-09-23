7deff786ed6e07cb796193b18c593374d4e29c6a
brenoacker <91087291+brenoacker@users.noreply.github.com>
2021-09-23 18:17:11 -0300

    Update segundo_ex.py

--- segundo_ex.py
+++ segundo_ex.py
@@ -1,10 +1,10 @@
-import requests, json
-
 #Pegar todas as 'siglas' de https://brasilapi.com.br/api/ibge/uf/v1
 #Colocar em uma lista todas as UFs
 #Fazer um loop que pegue todas as UFs em https://brasilapi.com.br/api/ibge/municipios/v1/{siglaUF}
 #dentro do loop, contar o numero de municipios
 #dentro do loop, fazer contagem de municipios
+
+import requests, json
 
 r = requests.get('https://brasilapi.com.br/api/ibge/uf/v1')
 r_dict = r.json()
@@ -15,7 +15,6 @@ i = 0
 j = 0
 k = 0
 
-#Pegar todas as siglas de estado
 while i < len(r_dict):
 
     sigla_local = r_dict[i]['sigla']
@@ -24,16 +23,11 @@ while i < len(r_dict):
     
     i = i + 1
 
-#Fazer o loop
-# filtrar pela cidade 
-# colocar um dict que tem a sigla e o numero de municipios (len())
-
 uf_numeromunicipios = {}
 while j < len(lista_siglas):
 
     r_local = requests.get(f'https://brasilapi.com.br/api/ibge/municipios/v1/{lista_siglas[j]}')
     r_dict_local = r_local.json()
-    #print(len(r_dict_local))
     
     sigla_local = lista_siglas[j]
     uf_numeromunicipios[sigla_local] = len(r_dict_local)
